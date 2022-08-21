81a3e6265ca33dc842a05ca5f945b298b5470317
Maxivich <maxivlevich@gmail.com>
2022-08-21 20:56:46 +0300

    HW

--- src/main/java/gb/ru/javafxchat/server/SqlBd.java
+++ src/main/java/gb/ru/javafxchat/server/SqlBd.java
@@ -1,17 +1,32 @@
 package gb.ru.javafxchat.server;
 import java.sql.*;
-import java.util.Properties;
-import java.util.logging.Logger;
+
 
 
 public class SqlBd {
 
     public static void main(String[] args) {
+
+        // CREATE TABLE users(
+        //      id INTEGER PRIMARY KEY AUTOINCREMENT,
+        //       name  VARCHAR(50),
+        //       pass  VARCHAR(50));
+        //
+        //
+        //
+        //
+        //      INSERT INTO users(name,pass)
+        //       VALUES('admin','admin');
+        //
+        //       SELECT*FROM users;
+
         Connection connection;
         Driver driver;
 
+
         try {
-            connection =DriverManager.getConnection("src/main/java/");
+            DriverManager.getConnection("jdbc:sqlite:src/main/resources/users.db");
+            connection = DriverManager.getConnection("src/main/java/");
 
 
 
